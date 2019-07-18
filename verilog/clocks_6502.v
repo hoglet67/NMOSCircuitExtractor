3010f65f61a0fb768ac0709a21bd1969773499f3
David Banks <dave@hoglet.com>
2019-07-18 09:31:51 +0100

    Ensure a 5 cycle reset pulse

--- verilog/clocks_6502.v
+++ verilog/clocks_6502.v
@@ -3,18 +3,20 @@
 // Copyright (c) 2010 Peter Monta
 
 module clocks_6502(input eclk, ereset, output reg res, output reg clk0);
-  reg [10:0] c;
+  reg [21:0] c;
   reg [7:0] i;
 
   always @(posedge eclk)
     if (ereset) begin
       c <= 0;
-      res <= 0;
+      res <= 1;
       clk0 <= 0;
       i <= 0;
     end else begin
       c <= c + 1;
-      if (c==11'd2047)
+      if (c==`HALFCYCLE * 5)
+        res <= 0;
+      if (c==`HALFCYCLE * 15)
         res <= 1;
       if (i==8'd`HALFCYCLE-1) begin
         i <= 0;
